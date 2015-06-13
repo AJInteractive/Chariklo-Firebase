b8f899c45d3fe411c0daafbdfcddef530ccb9f56
Laurian Gridinoc <laurian@gridinoc.name>
2015-06-13 21:51:55 +0100

    video, frames

---
+++ lib/contenteditable.jsx
@@ -0,0 +1,75 @@
+import $ from 'jquery';
+import React from 'react';
+
+var ContentEditable = React.createClass({
+  
+  html: '',
+  
+  render: function(){
+    
+    var html = '<p>';
+    var words = [];
+    
+    var tokens = this.props.text.split(' ');
+    var unit = (this.props.end - this.props.start)/tokens.lenght;
+        
+    for (var i = 0; i < tokens.length; i++) {
+    
+      words.push({
+        name: tokens[i],
+        time: this.props.start + i*unit
+      });
+      
+      var classes = '';
+      if (words[i].time < 0) classes += ' unedited';
+      if (words[i].time > -1 && words[i].time < this.props.time) classes += ' past';
+      html += '<span class="' + classes + '">' + tokens[i] + ' </span>';
+    }
+    
+    html += '</p>';    
+    this.html = html;
+    
+    return (
+      <div id="contenteditable"
+        onInput={this.emitChange} 
+        onBlur={this.emitChange}
+        contentEditable
+        dangerouslySetInnerHTML={{__html: html}}></div>
+      );
+  },
+
+  shouldComponentUpdate: function(nextProps){
+    var html = this.getDOMNode().innerHTML;
+    var text = $(html).text().trim();
+    
+    return (nextProps.text !== text 
+      || nextProps.start !== this.props.start 
+      || nextProps.end !== this.props.end 
+      || (nextProps.time !== this.props.time 
+        && (this.props.time > this.props.start && this.props.time < this.props.end)
+        && (nextProps.time > this.props.start && nextProps.time < this.props.end)
+      )
+    );
+  },
+  
+  // componentDidUpdate: function() {
+  //   if ( this.html !== this.getDOMNode().innerHTML ) {
+  //     this.getDOMNode().innerHTML = this.html;
+  //   }
+  // },
+
+  emitChange: function(event) {
+    var html = this.getDOMNode().innerHTML;
+    var text = $(html).text().trim();
+    if (this.props.onChange && html !== this.lastHtml && this.props.text !== text) {
+      event.target = { 
+        value: html,
+        text: text
+      };
+      this.props.onChange(event);
+    }
+    this.lastHtml = html;
+  }
+});
+
+export default ContentEditable;
--- lib/transcript.jsx
+++ lib/transcript.jsx
@@ -1,16 +1,19 @@
-import $ from 'jquery';
 import React from 'react';
 import ReactFireMixin from 'reactfire';
 import Firebase from 'firebase';
-import ContentEditable from 'react-contenteditable/index.js!jsx';
+// import ContentEditable from 'react-contenteditable/index.js!jsx';
+import ContentEditable from './contenteditable.jsx!';
+
+import $ from 'jquery';
+import interact from 'interact.js';
+import {VideoFrame} from 'X3TechnologyGroup/VideoFrame';
 
 ReactFireMixin._toArray = function(obj) {
   var out = [];
   if (obj) {
     if (this._isArray(obj)) {
       out = obj;
-    }
-    else if (typeof(obj) === "object") {
+    } else if (typeof(obj) === "object") {
       for (var key in obj) {
         if (obj.hasOwnProperty(key)) {
           var val = obj[key];
@@ -27,7 +30,22 @@ var Transcript = React.createClass({
   mixins: [ReactFireMixin],
   
   getInitialState: function() {
-    return {lines: []};
+    return {
+      lines: [],
+      time: 0,
+      smpte: ''
+    };
+  },
+  
+  // shouldComponentUpdate: function(nextProps, nextState) {
+  //   return (nextProps.id !== this.props.id || nextProps.lang !== this.props.lang);
+  // },
+  
+  componentWillReceiveProps: function(nextProps) {
+    if (nextProps.id !== this.props.id || nextProps.lang !== this.props.lang) {
+      this.unbind('lines');
+      this.bindAsArray(new Firebase('https://chariklo.firebaseio.com/transcript-' + nextProps.id + '-' + nextProps.lang), 'lines');
+    }
   },
   
   componentWillMount: function() {
@@ -36,30 +54,41 @@ var Transcript = React.createClass({
   
   componentWillUnmount: function() {
     this.unbind('lines');
+    if (this.media) {
+      this.media.removeEventListener( "timeupdate", this.mediaTimeupdate);
+    }
   },
     
-  handleChangeFor: function(key) {
+  handleTextChangeFor: function(key) {
     var self = this;
     return function(event) {      
       var lineRef = self.firebaseRefs.lines.child(key);
       lineRef.update({
-        text: $(event.target.value).text()
+        text: event.target.text
       });
     };
   },
   
   createLine: function(line, index) {
+    // return (
+    //   <div key={line.key} className={line.para?'paragraph':'line'}>
+    //     <ContentEditable html={'<p>' + line.text + '</p>'} onChange={this.handleChangeFor(line.key)} />
+    //   </div>
+    // );
     return (
       <div key={line.key} className={line.para?'paragraph':'line'}>
-        <ContentEditable html={'<p>' + line.text + '</p>'} onChange={this.handleChangeFor(line.key)} />
+        <ContentEditable text={line.text.trim()} start={line.start} end={line.end} onChange={this.handleTextChangeFor(line.key)} time={this.state.time} />
       </div>
     );
   },
       
   render: function() {
-    console.log(this.state);
     return (
-      <article className="transcript">
+      <article className={this.props.lang + ' transcript'}>
+        <div className="box stripes pace-hide" ref="box">
+          <span>{this.state.smpte}</span>
+          <video ref="media" controls src="http://player.vimeo.com/external/129669280.hd.mp4?s=e2b2185feabd68d101b37f5830c84404&profile_id=113"></video>
+        </div>
         <header>
           <h1>Transcript {this.props.id}-{this.props.lang}</h1>
         </header>
@@ -68,6 +97,73 @@ var Transcript = React.createClass({
         </section>
       </article>
     );
+  },
+  
+  componentDidMount: function() {
+    
+    var self = this;
+    this.media = this.refs.media.getDOMNode();    
+    this.mediaTimeupdate = function (event) {
+      var time = Math.round(1000*this.currentTime);
+      var smpte = '';
+      if (self.videoFrame) smpte = self.videoFrame.toSMPTE();
+      self.setState({time, smpte});
+    };
+    this.media.addEventListener('timeupdate', this.mediaTimeupdate);
+    
+    this.videoFrame = window.VideoFrame({
+      frameRate: 30
+    });
+    this.videoFrame.stopListen();
+    
+    /////////    
+    function dragMoveListener (event) {
+        var target = event.target,
+            // keep the dragged position in the data-x/data-y attributes
+            x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+            y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+        // translate the element
+        target.style.webkitTransform =
+        target.style.transform =
+          'translate(' + x + 'px, ' + y + 'px)';
+
+        // update the posiion attributes
+        target.setAttribute('data-x', x);
+        target.setAttribute('data-y', y);
+      }
+
+      // this is used later in the resizing demo
+      window.dragMoveListener = dragMoveListener;
+
+    interact('.box')
+      .draggable({
+        onmove: window.dragMoveListener
+      })
+      .resizable({
+        edges: { left: true, right: true, bottom: true, top: true }
+      })
+      .on('resizemove', function (event) {
+        var target = event.target,
+            x = (parseFloat(target.getAttribute('data-x')) || 0),
+            y = (parseFloat(target.getAttribute('data-y')) || 0);
+
+        // update the element's style
+        target.style.width  = event.rect.width + 'px';
+        target.style.height = event.rect.height + 'px';
+
+        // translate when resizing from top or left edges
+        x += event.deltaRect.left;
+        y += event.deltaRect.top;
+
+        target.style.webkitTransform = target.style.transform =
+            'translate(' + x + 'px,' + y + 'px)';
+
+        target.setAttribute('data-x', x);
+        target.setAttribute('data-y', y);
+        // target.textContent = event.rect.width + '×' + event.rect.height;
+      });
+    /////////
   }
   
 });
