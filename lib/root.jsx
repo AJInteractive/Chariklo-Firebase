898e445aaeb8b6400b0b60b091ccd93481cc5bd4
Laurian Gridinoc <laurian@gridinoc.name>
2015-06-13 01:40:22 +0100

    WIP

---
+++ lib/root.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+var Root = React.createClass({
+  render: function() {
+    return (
+      <h1>
+        Hello, world! 
+      </h1>
+    );
+  }
+});
+
+export Root;
